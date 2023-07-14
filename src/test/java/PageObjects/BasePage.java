67ffcb3a95a8e5c4ba7b2b15dc4a60c7edcf7e69
Becca <beccalangys26@gmail.com>
2023-07-13 20:10:35 -0500

    removing unneeded get and set methods for the driver. We do not use them.
    Signed-off-by: Becca <beccalangys26@gmail.com>

--- src/test/java/PageObjects/BasePage.java
+++ src/test/java/PageObjects/BasePage.java
@@ -12,14 +12,6 @@ public class BasePage {
     private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static WebDriver driver;
 
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(WebDriver driver) {
-        BasePage.driver = driver;
-    }
-
     public void waitForElementToLoad(By element) {
         //allowing for up to 10 checks for element load up. This allows for a few seconds of wait time.
         for (int i = 0; i < 10; i++) {
